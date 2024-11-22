8660250ef2ff866dc1c27bf9e72d5b813accebb7
Gustavo Justo <gustavo.justo02@gmail.com>
2024-11-22 17:39:24 -0300

    feat: return a book class in response

--- src/server.js
+++ src/server.js
@@ -3,11 +3,31 @@ import express from "express";
 
 dotenv.config();
 
+class BookResponse {
+  title;
+  author;
+  year;
+  genre;
+  constructor(title, author, year, genre) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.genre = genre;
+  }
+}
+
+const book = new BookResponse(
+  "The Lord of The Rings",
+  "J.R.R. Tolkien",
+  1954,
+  "Fantasy"
+);
+
 const app = express();
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server running on port ${process.env.SERVER_PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+app.get("/livro", (req, res) => {
+  res.status(200).send(book);
 });
